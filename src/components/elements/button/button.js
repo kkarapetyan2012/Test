2659780598fb23abe0e51e6d59327c83c6ec4b8f
kkarapetyan2012 <kkarapetyan2012@gmail.com>
2022-11-18 02:17:57 +0400

    second commit

--- src/components/elements/button/button.js
+++ src/components/elements/button/button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Button = ({ className, children, title, onClick}) => {
+const Button = ({ className, children, title, onClick, style}) => {
     return (
-        <button className={className} title={title} onClick={onClick}>
+        <button className={className} title={title} onClick={onClick} style={style}>
             {children}
         </button>
     );
--- src/components/tabs/MainTabs.js
+++ src/components/tabs/MainTabs.js
@@ -29,31 +29,55 @@ const MainTabs = () => {
     const toggleTab = (index) => setToggleTabs(index);
 
     return (
-        <div>
-            <div  className={`${toggleTabs === 1 ? 'active' : ''} tab-content`}>
-                <img src={i1} alt={`${i1}`} />
-            </div>
-            <div  className={`${toggleTabs === 2 ? 'active' : ''} tab-content`}>
-                <img src={i2} alt={`${i2}`} />
-            </div>
-            <div  className={`${toggleTabs === 3 ? 'active' : ''} tab-content`}>
-                <img src={i3} alt={`${i3}`} />
-            </div>
-            <div  className={`${toggleTabs === 4 ? 'active' : ''} tab-content`}>
-                <img src={i2} alt={`${i2}`} />
-            </div>
-            <div>
-                {tabs.map(tab =>
-                    <span
-                        className={`tab ${toggleTabs === tab.id ? 'active' : ''}`}
-                        key={tab.id}
-                        style={{background: `${tab.color}`}}
-                        onClick={() => toggleTab(tab.id)}
-                    />
+        <div className='tabs-container'>
+            <div className='container'>
+                <h2 className='tabs-title'>Choose your Beats</h2>
+                <div className='tabs-block'>
+                
+                    <div  className={`${toggleTabs === 1 ? 'active' : ''} tab-content`}>
+                        <img src={i1} alt={`${i1}`} />
+                    </div>
+                    <div  className={`${toggleTabs === 2 ? 'active' : ''} tab-content`}>
+                        <img src={i2} alt={`${i2}`} />
+                    </div>
+                    <div  className={`${toggleTabs === 3 ? 'active' : ''} tab-content`}>
+                        <img src={i3} alt={`${i3}`} />
+                    </div>
+                    <div  className={`${toggleTabs === 4 ? 'active' : ''} tab-content`}>
+                        <img src={i2} alt={`${i2}`} />
+                    </div>
+                    <div className='tab-heading'>
+                        <h3 className='tab-title'>Beats Fit Pro</h3>
+                        <p className='tab-paragraph'>$199.99</p>
+                        <ul className='info-list'>
+                            <li className='info-item'>Flexible, secure-fit wingtips</li>
+                            <li className='info-item'>Active Noise Cancelling (ANC) and Transparency</li>
+                            <li className='info-item'>Up to 6 hours of listening time with ANC on</li>
+                            <li className='info-item'>Autoplay/Pause connection via Apple H1 chip</li>
+                            <li className='info-item'>Flexible, secure-fit wingtips</li>
+                            <li className='info-item'>Flexible, secure-fit wingtips</li>
+                        </ul>
+                        
+                        <div className='buttons-block'>
+                            <p className='buttons-text'>{tabs.length} color</p>
+                            <p className='buttons-list'>
+                                {tabs.map(tab =>
+                                    <Button
+                                        className={`tab-button ${toggleTabs === tab.id ? 'active' : ''}`}
+                                        key={tab.id}
+                                        style={{background: `${tab.color}`}}
+                                        onClick={() => toggleTab(tab.id)}
+                                    />
+                                )}
+                            </p>
+                        </div>
 
-                )}
+                        <Button>Button</Button>
+                        
+                    </div>
+                </div>
             </div>
-
+            
         </div>
     );
 };
